240cd867d87d84ed88f4efc35b6b8a35aaa0476f
isvogor <isvogor@foi.hr>
2015-10-28 19:52:45 +0100

    ...

---
+++ FinalMeasurement/src/hr/foi/thor/main/scenario1/FilteringInitializer.java
@@ -0,0 +1,24 @@
+package hr.foi.thor.main.scenario1;
+
+import hr.foi.thor.InitializerS1;
+
+public class FilteringInitializer {
+
+	public static void run(){
+		
+		// S-VGA
+		InitializerS1 i = new InitializerS1(0);
+		i.runSVga(i.createBufferedImage("/home/ivan/Dev/java/temp/cap2g.jpg"));
+		
+		// SGE-VGA
+		i.runSgeVga(i.createBufferedImage("/home/ivan/Dev/java/temp/cap2g.jpg"));
+
+		// SGE-SXGA
+		i.runSgeSxga(i.createBufferedImage("/home/ivan/Dev/java/temp/cap2g.jpg"));
+
+		// SGEDH-SXGA
+		i.runSgedhSxga(i.createBufferedImage("/home/ivan/Dev/java/temp/cap2g.jpg"));
+
+	}
+	
+}
