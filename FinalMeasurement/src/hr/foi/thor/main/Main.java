e17e67a4758abd2e959190869c377cf76ca14fcf
isvogor <isvogor@foi.hr>
2015-10-28 20:22:07 +0100

    Calls added for IF component

--- FinalMeasurement/src/hr/foi/thor/main/Main.java
+++ FinalMeasurement/src/hr/foi/thor/main/Main.java
@@ -1,10 +1,16 @@
 package hr.foi.thor.main;
 
+import hr.foi.thor.main.scenario1.FilteringInitializer;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world...");
+		
+		FilteringInitializer.run();
+		
+		
 	}
 
 }
--- FinalMeasurement/src/hr/foi/thor/main/scenario1/FilteringInitializer.java
+++ FinalMeasurement/src/hr/foi/thor/main/scenario1/FilteringInitializer.java
@@ -6,6 +6,7 @@ public class FilteringInitializer {
 
 	public static void run(){
 		
+		// TODO: set image sizes..
 		// S-VGA
 		InitializerS1 i = new InitializerS1(0);
 		i.runSVga(i.createBufferedImage("/home/ivan/Dev/java/temp/cap2g.jpg"));
@@ -14,6 +15,7 @@ public class FilteringInitializer {
 		i.runSgeVga(i.createBufferedImage("/home/ivan/Dev/java/temp/cap2g.jpg"));
 
 		// SGE-SXGA
+		i.initialize();
 		i.runSgeSxga(i.createBufferedImage("/home/ivan/Dev/java/temp/cap2g.jpg"));
 
 		// SGEDH-SXGA
